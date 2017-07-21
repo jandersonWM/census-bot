f367a175404d65625521c59a640e0eb67b33ef16
Jack Anderson <jeanderson@email.wm.edu>
2017-07-21 15:34:38 -0400

    Initial commit of bot

---
+++ CensusBot/DataTypes/CensusDataTypes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//TODO: move to a data layer project
+namespace Bot_Application1.DataTypes
+{
+    [Serializable]
+    public class CensusDataTypes
+    {
+        public List<string> LevelsOfGeography { get; set; }
+        public List<string> States { get; set; }
+        public string LevelOfGeography { get; set; }
+        public bool ResolveSuccess { get; set; }
+        public string Message { get; set; }
+
+        public CensusDataTypes()
+        {
+            LevelsOfGeography = new List<string>()
+            {
+                "nation", "country", "division", "region", "state", "county", "city", "census tract",
+                "tract", "block", "congressional district", "cities", "counties", "bushes"
+            };
+            States = new List<string>()
+            {
+                "alabama", "alaska", "arizona", "arkansas", "california", "colorado", "connecticut",
+                "delaware","florida", "georgia", "district of columbia", "washington dc", "hawaii",
+                "idaho","illinois","indiana", "iowa", "kansas", "kentucky", "louisiana", "maine",
+                "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri",
+                "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico",
+                "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon",
+                "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee",
+                "texas", "utah", "vermont", "virginia", "washington", "west virginia",
+                "wisconsin", "wyoming"
+            };
+        }
+    }
+}
---
+++ CensusBot/DataTypes/TableNumberLookup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//TODO: move to a data layer project
+namespace Bot_Application1.DataTypes
+{
+    public static class TableNumberLookup
+    {
+        public static List<Tuple<string, string>> Incomes {get; set; }
+        static TableNumberLookup()
+        {
+            //keys represent the minimum income for the subject number
+            //ranges could call all tuples with keys in the range
+            Incomes = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("all", "001E"), new Tuple<string, string>("0", "002E"),
+                new Tuple<string, string>("10000", "003E"), new Tuple<string, string>("15000", "004E"),
+                new Tuple<string, string>("20000", "005E"), new Tuple<string, string>("25000", "006E"),
+                new Tuple<string, string>("30000", "007E"), new Tuple<string, string>("35000", "008E"),
+                new Tuple<string, string>("40000", "009E"), new Tuple<string, string>("45000", "010E"),
+                new Tuple<string, string>("50000", "011E"), new Tuple<string, string>("60000", "012E"),
+                new Tuple<string, string>("75000", "013E"), new Tuple<string, string>("100000", "014E"),
+                new Tuple<string, string>("125000", "015E"), new Tuple<string, string>("150000", "016E"),
+                new Tuple<string, string>("200000", "017E")
+            };
+        }
+    }
+}
